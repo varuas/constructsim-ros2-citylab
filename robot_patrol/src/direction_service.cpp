a7f9ec170c2b978814abc5de12b44a49c982f0d0
Saurav Saha <varuas@gmail.com>
2023-11-09 11:58:57 +0000

    Updated the algo by considering min dist in addition to total dist

--- robot_patrol/src/direction_service.cpp
+++ robot_patrol/src/direction_service.cpp
@@ -27,8 +27,11 @@ private:
     float total_dist_sec_right = 0.0;
     float total_dist_sec_front = 0.0;
     float total_dist_sec_left = 0.0;
+    float nearest_left = 99;
+    float nearest_front = 99;
+    float nearest_right = 99;
     float dist = 0;
-    float max_dist = 2.0;
+    float max_dist = 1.0;
 
     // Right
     for (int i = 180; i < 300; i++) {
@@ -39,6 +42,9 @@ private:
         dist = request->laser_data.ranges[i];
       }
       total_dist_sec_right += dist;
+      if (request->laser_data.ranges[i] < nearest_right) {
+        nearest_right = request->laser_data.ranges[i];
+      }
     }
 
     // Front
@@ -50,6 +56,9 @@ private:
         dist = request->laser_data.ranges[i];
       }
       total_dist_sec_front += dist;
+      if (request->laser_data.ranges[i] < nearest_front) {
+        nearest_front = request->laser_data.ranges[i];
+      }
     }
 
     // Left
@@ -61,21 +70,35 @@ private:
         dist = request->laser_data.ranges[i];
       }
       total_dist_sec_left += dist;
+      if (request->laser_data.ranges[i] < nearest_left) {
+        nearest_left = request->laser_data.ranges[i];
+      }
     }
 
-    RCLCPP_INFO(this->get_logger(), "Left [%f]   Front [%f]   Right [%f]",
-                total_dist_sec_left, total_dist_sec_front,
-                total_dist_sec_right);
+    RCLCPP_INFO(
+        this->get_logger(), "TOTAL : Left [%f]   Front [%f]   Right [%f]",
+        total_dist_sec_left, total_dist_sec_front, total_dist_sec_right);
+    RCLCPP_INFO(this->get_logger(),
+                "NEAREST : Left [%f]   Front [%f]   Right [%f]", nearest_left,
+                nearest_front, nearest_right);
 
-    if (total_dist_sec_right > total_dist_sec_front &&
-        total_dist_sec_right > total_dist_sec_left) {
-      response->direction = "right";
-    } else if (total_dist_sec_front > total_dist_sec_right &&
-               total_dist_sec_front > total_dist_sec_left) {
+    if (nearest_left > nearest_front && nearest_left > nearest_right) {
+      response->direction = "left";
+    } else if (nearest_front > nearest_left && nearest_front > nearest_right) {
       response->direction = "forward";
     } else {
-      response->direction = "left";
+      response->direction = "right";
     }
+
+    // if (total_dist_sec_right > total_dist_sec_front &&
+    //     total_dist_sec_right > total_dist_sec_left) {
+    //   response->direction = "right";
+    // } else if (total_dist_sec_front > total_dist_sec_right &&
+    //            total_dist_sec_front > total_dist_sec_left) {
+    //   response->direction = "forward";
+    // } else {
+    //   response->direction = "left";
+    // }
 
     RCLCPP_INFO(this->get_logger(), "Direction: %s",
                 response->direction.c_str());
