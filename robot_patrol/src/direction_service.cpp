0450a372db6fb9e43bc650792a932220257e4ef1
Saurav Saha <varuas@gmail.com>
2023-11-09 11:24:25 +0000

    Optimized the algo by better handling of far obstacles

--- robot_patrol/src/direction_service.cpp
+++ robot_patrol/src/direction_service.cpp
@@ -27,35 +27,45 @@ private:
     float total_dist_sec_right = 0.0;
     float total_dist_sec_front = 0.0;
     float total_dist_sec_left = 0.0;
+    float dist = 0;
+    float max_dist = 2.0;
 
     // Right
     for (int i = 180; i < 300; i++) {
-      if (std::isinf(request->laser_data.ranges[i])) {
-        total_dist_sec_right += request->laser_data.range_max;
+      if (std::isinf(request->laser_data.ranges[i]) ||
+          request->laser_data.ranges[i] > max_dist) {
+        dist = max_dist;
       } else {
-        total_dist_sec_right += request->laser_data.ranges[i];
+        dist = request->laser_data.ranges[i];
       }
+      total_dist_sec_right += dist;
     }
 
     // Front
     for (int i = 300; i < 420; i++) {
-      if (std::isinf(request->laser_data.ranges[i])) {
-        total_dist_sec_front += request->laser_data.range_max;
+      if (std::isinf(request->laser_data.ranges[i]) ||
+          request->laser_data.ranges[i] > max_dist) {
+        dist = max_dist;
       } else {
-        total_dist_sec_front += request->laser_data.ranges[i];
+        dist = request->laser_data.ranges[i];
       }
+      total_dist_sec_front += dist;
     }
 
     // Left
     for (int i = 420; i < 540; i++) {
-      if (std::isinf(request->laser_data.ranges[i])) {
-        total_dist_sec_left += request->laser_data.range_max;
+      if (std::isinf(request->laser_data.ranges[i]) ||
+          request->laser_data.ranges[i] > max_dist) {
+        dist = max_dist;
       } else {
-        total_dist_sec_left += request->laser_data.ranges[i];
+        dist = request->laser_data.ranges[i];
       }
+      total_dist_sec_left += dist;
     }
 
-    RCLCPP_INFO(this->get_logger(), "Left [%f]   Front [%f]   Right [%f]", total_dist_sec_left, total_dist_sec_front, total_dist_sec_right);
+    RCLCPP_INFO(this->get_logger(), "Left [%f]   Front [%f]   Right [%f]",
+                total_dist_sec_left, total_dist_sec_front,
+                total_dist_sec_right);
 
     if (total_dist_sec_right > total_dist_sec_front &&
         total_dist_sec_right > total_dist_sec_left) {
@@ -67,7 +77,8 @@ private:
       response->direction = "left";
     }
 
-      RCLCPP_INFO(this->get_logger(), "Direction: %s", response->direction.c_str());
+    RCLCPP_INFO(this->get_logger(), "Direction: %s",
+                response->direction.c_str());
   }
 };
 
